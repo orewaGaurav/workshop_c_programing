0b26ebab0130f9f7b4f6f96d103458359a1f5142
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-08-30 00:29:16 +0530

    Fibo

--- fibo.c
+++ fibo.c
@@ -14,21 +14,22 @@ int main(){
 
 }
 
+// // using recursion 
 // #include <stdio.h>
-// int main(){
-//     int n;
-//     int t1 = 0;
-//     int t2 = 1;
-//     int next_term = t1+t2;
-//     printf("Enter the n:- ");
-//     scanf("%d",&n);
-//     printf("Fibo series %d ,%d, ",t1,t2);
-//     for(int i=0;i<=n;i++){
-//         printf("%d, ",next_term);
-//         t1 = t2;
-//         t2=next_term;
-//         next_term = t1+t2;
-        
+// int fibo(int n){
+//     if(n==0 || n==1){
+//         return n;
 //     }
-    
+//     else{
+//         return fibo(n-1)+fibo(n-2);
+//     }
+// }
+// int main(){
+//     int n,i;
+//     printf("Enter the no. of term: ");
+//     scanf("%d",&n);
+//     printf("fibonacci series: ");
+//     for(i=0;i<n;i++){
+//         printf("%d ",fibo(i));
+//     }
 // }
