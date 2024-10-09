59b2e97778bfdffb3ebdaa5a93abfeb5034d41f1
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-10-09 23:52:54 +0530

    linked list

---
+++ linked-list-create-display.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+//self referential structure
+struct node {
+    int data;
+    struct node *next;
+} *head = NULL;
+
+//function to create node
+void create(){
+    struct node *ptr,*tmp;
+    ptr = (struct node*)malloc(sizeof(struct node));
+    ptr->next = NULL;
+    printf("Enter the value: \n");
+    scanf("%d",&ptr->data);
+    //check if its first node or not
+    if(head == NULL){
+        head = ptr;
+    }
+    else{
+        tmp = head;
+        while(tmp->next != NULL){
+        tmp = tmp->next;
+        }
+        tmp->next = ptr;
+
+    }
+}
+//function  to display nodes
+void display(){
+    struct node *new;
+    new = head;
+    if(head == NULL)
+    printf("No node exist");
+    else{
+        while(new != NULL){
+        printf("%d ",new->data);
+        new = new->next;
+        }
+    }
+}
+//main function
+int main(){
+    int choice;
+     while(1){
+    printf("\n1\tCreate node\n2\tDisplay node\n3\tExit\nEnter your choice: ");
+    scanf("%d",&choice);
+   
+        if(choice == 1)
+        create();
+        else if(choice == 2)
+        display();
+        else if(choice == 3)
+        break;
+        else
+        printf("Enter the right choice: 1\t2\t3");
+    }
+}
