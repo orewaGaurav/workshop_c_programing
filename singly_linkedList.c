cc4f230d9d5d00f2ca132d4d10b0f4e8dfc4e52a
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-10-12 18:15:37 +0530

    l

--- singly_linkedList.c
+++ singly_linkedList.c
@@ -184,6 +184,6 @@ int main(){
         else if(choice == 10)
         delete();
         else
-        printf("Enter the right choice: 1 2 3 4 5 6 7");
+        printf("Enter the right choice: 1 2 3 4 5 6 7 8 9 10");
     }
 }
