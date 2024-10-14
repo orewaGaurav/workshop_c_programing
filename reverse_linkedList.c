a52b0b9dfc24fdb4c432701f9a8c88e8e95211ff
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-10-14 15:11:54 +0530

    reverse linked list

---
+++ reverse_linkedList.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *next;
+}*head = NULL;
+
+void insert(int value){
+    struct node *ptr;
+    ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data=value;
+    ptr->next = head;
+    head=ptr;
+}
+
+void display(){
+    struct node *tmp,*pre;
+    tmp = head;
+    while(tmp != NULL){
+        printf("%d ",tmp->data);
+        tmp=tmp->next;
+    }
+    printf("\n");
+}
+
+int main(){
+    int n,value;
+    printf("Enter the no. f nodes: ");
+    scanf("%d",&n);
+    for(int i = 0;i<n;i++){
+        scanf("%d",&value);
+        insert(value);
+    }
+    display();
+}
